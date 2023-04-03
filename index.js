5335669dd2841fdea5ed369432c35b2d00b1482b
Breki Ingibjargarson <breki@directivegames.com>
2023-04-03 19:25:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+users = {}
+
 app.post('/users', (req, res) => {
-  const { name, email } = req.body;
-  // perform any necessary processing on the data
+  const { name, score } = req.body;
+  users.push({
+    "name": name,
+    "score": score
+  })
   res.send('User created successfully');
 });
 
+app.get('/users', (req, res) => {
+  res.send(users)
+});
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
